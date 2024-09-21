e173b7d8213702ec5f4292ad043f0f4d5c978b87
divi-eek <cdivyanshi2003@gmail.com>
2024-09-21 21:08:34 +0530

    db

--- Db/connectDB.js
+++ Db/connectDB.js
@@ -4,7 +4,7 @@ const localUrl="mongodb://127.0.0.1:27017/blogProject"
 const liveUrl ="mongodb+srv://pn123:pn123@cluster0.ygpytkl.mongodb.net/blogproject?retryWrites=true&w=majority&appName=Cluster0"
 
 const connect=()=>{
-    mongoose.connect(localUrl).then(()=>{
+    mongoose.connect(liveUrl).then(()=>{
         console.log('connect success')
     }).catch((err)=>{
         console.log(err)
